6629d00efd70e7b9e21e2236c511125f85a019bf
agg-mb <maximilian.brand@agilimo.de>
2024-01-02 16:51:05 +0100

    fix time zone

--- fetchrss.py
+++ fetchrss.py
@@ -1,11 +1,25 @@
-import requests
+import datetime
+import locale
 import xml.etree.ElementTree as ET
+import requests
 import re
 
-# Function to extract image URL from description
-def extract_image_url(description):
-    match = re.search(r'<img.*?src="(.*?)"', description)
-    return match.group(1) if match else None
+from datetime import datetime
+from pytz import timezone
+from babel.dates import format_date, format_time, get_timezone
+
+# Function to convert and format date to Berlin timezone in German
+def format_date_to_berlin_time(date_string):
+    original_format = '%a, %d %b %Y %H:%M:%S %z'
+    berlin = timezone('Europe/Berlin')
+    date_obj = datetime.strptime(date_string, original_format)
+    date_obj_berlin = date_obj.astimezone(berlin)
+
+    # Format date and time separately
+    formatted_date = format_date(date_obj_berlin, format='full', locale='de_DE')
+    formatted_time = format_time(date_obj_berlin, format='short', locale='de_DE', tzinfo=get_timezone('Europe/Berlin'))
+
+    return f"{formatted_date} {formatted_time} Uhr"
 
 def clean_html(raw_html):
     cleanr = re.compile('<.*?>')
@@ -28,12 +42,12 @@ with open('rss_content.txt', 'w') as file:
         title = item.find('title').text
         link = item.find('link').text
         pub_date = item.find('pubDate').text if item.find('pubDate') is not None else 'No date'
-        
-        # Extract image URL from description
+        pub_date = format_date_to_berlin_time(pub_date) if item.find('pubDate') != 'No date' else 'No date'
+
+        # Extract URL from description
         description = item.find('description').text if item.find('description') is not None else ''
-        image_url = extract_image_url(description)
 
         # Clean HTML tags from description
         clean_description = clean_html(description)
 
-        file.write(f"Title: {title}\nLink: {link}\nPublication Date: {pub_date}\nImage URL: {image_url}\nDescription: {clean_description}\n\n")
+        file.write(f"Title: {title}\nLink: {link}\nPublication Date: {pub_date}\nDescription: {clean_description}\n\n")
