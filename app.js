87dc05f09c1bcbc3b8c9853e8c5897de21f0bb04
agg-mb <134610601+agg-mb@users.noreply.github.com>
2024-04-08 15:04:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -115,3 +115,8 @@ function sanitizeHTML(str) {
     }
     return temp.innerHTML; // return sanitized HTML
 }
+
+document.addEventListener('click', function(event) {
+    // Prevent default action for all clicks except those on the scrollbar
+    event.preventDefault();
+}, true);
